ef706a20f3b1ca2130f71a2ac0398d68e88be593
tkugimot <toshimitsu_kugimoto@yahoo.co.jp>
2020-09-24 01:22:13 +0900

    Add vuetify and carouesl

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,76 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+<!--    <v-app-bar-->
+<!--      app-->
+<!--      color="primary"-->
+<!--      dark-->
+<!--    >-->
+<!--      <div class="d-flex align-center">-->
+<!--        <v-img-->
+<!--          alt="Vuetify Logo"-->
+<!--          class="shrink mr-2"-->
+<!--          contain-->
+<!--          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"-->
+<!--          transition="scale-transition"-->
+<!--          width="40"-->
+<!--        />-->
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+<!--        <v-img-->
+<!--          alt="Vuetify Name"-->
+<!--          class="shrink mt-1 hidden-sm-and-down"-->
+<!--          contain-->
+<!--          min-width="100"-->
+<!--          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"-->
+<!--          width="100"-->
+<!--        />-->
+<!--      </div>-->
 
-      <v-spacer></v-spacer>
+<!--      <v-spacer></v-spacer>-->
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+<!--      <v-btn-->
+<!--        href="https://github.com/vuetifyjs/vuetify/releases/latest"-->
+<!--        target="_blank"-->
+<!--        text-->
+<!--      >-->
+<!--        <span class="mr-2">Latest Release</span>-->
+<!--        <v-icon>mdi-open-in-new</v-icon>-->
+<!--      </v-btn>-->
+<!--    </v-app-bar>-->
 
     <v-main>
-      <HelloWorld/>
+      <Carousel/>
+<!--      <HelloWorld/>-->
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
+import Carousel from "./components/Carousel";
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Carousel,
+    // HelloWorld,
   },
 
   data: () => ({
-    //
+    colors: [
+      'indigo',
+      'warning',
+      'pink darken-2',
+      'red lighten-1',
+      'deep-purple accent-4',
+    ],
+    slides: [
+      'First',
+      'Second',
+      'Third',
+      'Fourth',
+      'Fifth',
+    ],
   }),
 };
 </script>
